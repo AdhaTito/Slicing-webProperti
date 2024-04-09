85d4b538fbc4e1b37b7f9b97cabe509bc12a9217
Zitcheron <adhamastito55@gmail.com>
2024-04-09 14:28:33 +0700

    secound commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./src/**/*.{html,js}", "./dist/flowbite.min.js"],
+  content: ["index.html", "./dist/flowbite.min.js"],
   theme: {
     container: {
       center: true,
